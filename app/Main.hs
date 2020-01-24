128fa5dfc7facc09345e12319eafcbe6c39f7443
Michael Tsang <michaeltsang1998@gmail.com>
2020-01-24 22:39:25 +0000

    Add file parser

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,52 @@ module Main where
 
 import Simple.Parser (parseProg)
 
+import System.Environment
+import System.Console.GetOpt
+import Control.Monad
 import Control.Monad.Trans
 import System.Console.Haskeline
 
+data Flag = Interactive | File String
+    deriving (Show)
+
+options :: [OptDescr Flag]
+options = []
+    -- [ Option ['i'] ["interactive"] (NoArg Interactive) "interactive mode"
+    -- , Option ['f'] ["file"] (ReqArg File "FILE") "parse a file"
+    -- ]
+
 process :: String -> IO ()
 process input =
-  case parseProg input of
-    Left err -> do
-        putStrLn "Parse Error:"
-        print err
-    Right ast -> 
-        print ast
+    case parseProg input of
+        Left err -> do
+            putStrLn "Parse Error:"
+            print err
+        Right ast -> 
+            print ast
+
+parseArgs :: [String] -> IO ([Flag], [String])
+parseArgs argv = 
+    case getOpt Permute options argv of
+        (o,n,[]  ) -> return (o,n)
+        (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = "Usage: ic [OPTION...] files..."
 
 main :: IO ()
-main = runInputT defaultSettings loop
-  where
-    loop = do
-        minput <- getInputLine "Happy> "
-        case minput of
-            Nothing -> outputStrLn "Goodbye."
-            Just input -> liftIO (process input) >> loop
+main = do
+    (_, fs) <- getArgs >>= parseArgs
+    case fs of
+        [] -> 
+            runInputT defaultSettings loop
+        files -> 
+            processAll files
+    where
+        loop = do
+            minput <- getInputLine "Happy> "
+            case minput of
+                Nothing -> outputStrLn "Goodbye."
+                Just input -> liftIO (process input) >> loop
+        processAll [] = putStrLn "Done."
+        processAll (x : xs) = do 
+            contents <- readFile x
+            liftIO (process contents) >> processAll xs
