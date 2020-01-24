73c3f44ebba24ce331b4bdc581556606c5d485a0
Michael Tsang <michaeltsang1998@gmail.com>
2020-01-24 22:57:25 +0000

    Add parser tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Simple.Parser (parseProg)
+import Simple.Syntax
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [ parserTests ]
+
+parserTests = testGroup "Parser tests"
+    [ testCase "function definition literal" $
+        parseProg "a = 1;" @?= Right [Func "a" [] (Lit (LInt 1))]
+    , testCase "multiple function definition" $
+        parseProg "a = 1;\nb = 2;" @?= 
+            Right [ Func "a" [] (Lit (LInt 1))
+                  , Func "b" [] (Lit (LInt 2))
+                  ]
+    , testCase "function definition addition" $
+        parseProg "a = 1 + 2;" @?= 
+            Right [Func "a" [] (Op Add (Lit (LInt 1)) (Lit (LInt 2)))]
+    , testCase "function definition application" $
+        parseProg "a = b c d;" @?= 
+            Right [Func "a" [] (App (App (Var "b") (Var "c")) (Var "d"))]
+    ]
