72e93d004799840fed5411a24972dbb9407c1eb5
Michael Tsang <michaeltsang1998@gmail.com>
2020-04-14 22:52:08 +0100

    aggregate tests

--- src/Frontend/Aggregator.hs
+++ src/Frontend/Aggregator.hs
@@ -14,30 +14,30 @@ type AggregationTable = Map Name Aggregation
 
 aggregate :: Prog -> Either Error AggregationTable
 aggregate = foldM aggregate' Map.empty
-
-aggregate' :: AggregationTable ->  Decl -> Either Error AggregationTable 
-aggregate' at (Func name params defn)
-    = case Map.lookup name at of
-        Just (mcplx, mts, Nothing) ->
-            return $ Map.insert name (mcplx, mts, Just (params, defn)) at
-        Just (_, _, Just _) ->
-            throwError DuplicateDeclaration
-        Nothing ->
-            return $ Map.insert name (Nothing, Nothing, Just (params, defn)) at
-aggregate' at (Cplx name c) = do
-    cplx <- parseComplexity c
-    case Map.lookup name at of
-        Just (Nothing, mts, mfunc) ->
-            return $ Map.insert name (Just cplx, mts, mfunc) at
-        Just (Just _, _, _) ->
-            throwError DuplicateDeclaration
-        Nothing ->
-            return $ Map.insert name (Just cplx, Nothing, Nothing) at
-aggregate' at (Type name ts)
-    = case Map.lookup name at of
-        Just (mcplx, Nothing, mfunc) ->
-            return $ Map.insert name (mcplx, Just ts, mfunc) at
-        Just (_, Just _, _) ->
-            throwError DuplicateDeclaration
-        Nothing ->
-            return $ Map.insert name (Nothing, Just ts, Nothing) at
+    where
+        aggregate' :: AggregationTable ->  Decl -> Either Error AggregationTable 
+        aggregate' at (Func name params defn)
+            = case Map.lookup name at of
+                Just (mcplx, mts, Nothing) ->
+                    return $ Map.insert name (mcplx, mts, Just (params, defn)) at
+                Just (_, _, Just _) ->
+                    throwError DuplicateDeclaration
+                Nothing ->
+                    return $ Map.insert name (Nothing, Nothing, Just (params, defn)) at
+        aggregate' at (Cplx name c) = do
+            cplx <- parseComplexity c
+            case Map.lookup name at of
+                Just (Nothing, mts, mfunc) ->
+                    return $ Map.insert name (Just cplx, mts, mfunc) at
+                Just (Just _, _, _) ->
+                    throwError DuplicateDeclaration
+                Nothing ->
+                    return $ Map.insert name (Just cplx, Nothing, Nothing) at
+        aggregate' at (Type name ts)
+            = case Map.lookup name at of
+                Just (mcplx, Nothing, mfunc) ->
+                    return $ Map.insert name (mcplx, Just ts, mfunc) at
+                Just (_, Just _, _) ->
+                    throwError DuplicateDeclaration
+                Nothing ->
+                    return $ Map.insert name (Nothing, Just ts, Nothing) at
--- test/FrontendTests.hs
+++ test/FrontendTests.hs
@@ -5,16 +5,19 @@ import Test.Tasty.HUnit
 
 import Prelude hiding (EQ, LT, GT)
 import Data.Either (Either(..))
+import qualified Data.Map.Strict as Map
 
 import Simple.Syntax
 import Frontend.Complexity (parseComplexity, Cplx(..))
 import Frontend.Verification (verifyAggregation)
+import Frontend.Aggregator (aggregate)
 import Frontend.Error (Error(..))
 
 frontendTests :: TestTree
 frontendTests = testGroup "Frontend Tests"
     [ parseComplexityTests
     , verifyAggregationTests
+    , aggregateTests
     ]
 
 parseComplexityTests = testGroup "parseComplexity tests"
@@ -72,4 +75,54 @@ verifyAggregationTests = testGroup "verifyAggregation tests"
     , testCase "complexity with param present in function params is incompatible (untyped)" $
         verifyAggregation (Just (Polynomial "n" 2), Nothing, Just (["n"], Lit (LInt 1)))
             @?= Right ()
+    ]
+
+basicFuncDefn = Op Add (Lit (LInt 1)) (Lit (LInt 1))
+basicFuncDecl = Func "foo" ["n"] basicFuncDefn
+basicCplxDecl = Cplx "foo" (Var "n")
+basicTypeDecl = Type "foo" [Int, Int]
+
+aggregateTests = testGroup "aggregate tests"
+    [ testCase "forms complete when given cplx, type, func" $
+        aggregate [basicCplxDecl, basicTypeDecl, basicFuncDecl]
+            @?= Right (Map.fromList [("foo", (Just (Polynomial "n" 1), Just [Int, Int], Just (["n"], basicFuncDefn)))])
+    , testCase "forms complete when given type, cplx, func" $
+        aggregate [basicTypeDecl, basicCplxDecl, basicFuncDecl]
+            @?= Right (Map.fromList [("foo", (Just (Polynomial "n" 1), Just [Int, Int], Just (["n"], basicFuncDefn)))])
+    , testCase "forms complete when given func, cplx, type" $
+        aggregate [basicFuncDecl, basicCplxDecl, basicTypeDecl]
+            @?= Right (Map.fromList [("foo", (Just (Polynomial "n" 1), Just [Int, Int], Just (["n"], basicFuncDefn)))])
+    , testCase "forms partial when given func, type" $
+        aggregate [basicFuncDecl, basicTypeDecl]
+            @?= Right (Map.fromList [("foo", (Nothing, Just [Int, Int], Just (["n"], basicFuncDefn)))])
+    , testCase "forms partial when given cplx, type" $
+        aggregate [basicCplxDecl, basicTypeDecl]
+            @?= Right (Map.fromList [("foo", (Just (Polynomial "n" 1), Just [Int, Int], Nothing))])
+    , testCase "forms only func when given func" $
+        aggregate [basicFuncDecl]
+            @?= Right (Map.fromList [("foo", (Nothing, Nothing, Just (["n"], basicFuncDefn)))])
+    , testCase "fails when duplicate declarations present" $
+        aggregate [basicFuncDecl, basicTypeDecl, Type "foo" [Int, Bool]]
+            @?= Left DuplicateDeclaration
+    , testCase "forms multiple entries for different function declarations" $
+        aggregate 
+            [ basicFuncDecl
+            , basicTypeDecl
+            , basicCplxDecl
+            , Type "bar" [Bool, Int]
+            , Cplx "bar" (Var "m")
+            , Func "baz" ["n"] basicFuncDefn
+            , Cplx "baz" (Op Exp (Lit (LInt 2)) (Var "q"))
+            ]
+            @?= Right (Map.fromList
+                [ ("foo", (Just (Polynomial "n" 1), Just [Int, Int], Just (["n"], basicFuncDefn)))
+                , ("bar", (Just (Polynomial "m" 1), Just [Bool, Int], Nothing))
+                , ("baz", (Just (Exponential 2 "q"), Nothing, Just (["n"], basicFuncDefn)))
+                ])
+    , testCase "fails when complexity uses illegal expressions" $
+        aggregate [Cplx "foo" (If (Lit (LBool True)) (Var "n") (Op Exp (Var "n") (Lit (LInt 2))))]
+        @?= Left IllegalComplexity
+    , testCase "fails when complexity is uses unsupported expressions" $
+        aggregate [Cplx "foo" (Op Add (Var "n") (Var "m"))]
+        @?= Left UnsupportedComplexity
     ]
